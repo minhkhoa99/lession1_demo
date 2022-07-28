8459746c9f2269c8a9aa7b3563befcff2b858b6f
minhkhoa99 <110026964+minhkhoa99@users.noreply.github.com>
2022-07-28 22:25:51 +0700

    BTVN buổi 1

---
+++ less1/main.js
@@ -0,0 +1,27 @@
+//bt1:
+// let vatLy = parseInt(prompt("Nhập điểm môn Lý:"));
+// let hoaHoc = parseInt(prompt('Nhập điểm môn Hóa:'));
+// let sinhHoc = parseInt(prompt("Nhập điểm môn Sinh:"));
+
+// tong = (vatLy + hoaHoc + sinhHoc )/3;
+
+// alert('Trung bình cộng của các môn học là: '+ tong);
+//bt2:
+
+// let doC = parseInt(prompt("Nhập độ C:"));
+
+// let doF = (1.8*doC) + 32;
+
+// alert("Nhiệt độ chuyển sang là: "+ doF);
+
+//bt3 + 4:
+
+let circle = parseInt(prompt('Nhập bán kính hình tròn:'));
+
+let r = 3.14* circle**2;
+
+let chuVi = 2*3.14*circle;
+
+alert("Diện tích hình tròn là: "+ r);
+
+alert("Chu vi hình tròn là: "+ chuVi);
