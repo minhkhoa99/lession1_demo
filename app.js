13fbbb2575763bd8f1989b60ceb473f6d95c40bc
minhkhoa99 <110026964+minhkhoa99@users.noreply.github.com>
2022-07-28 17:04:03 +0700

    Up load file

---
+++ app.js
@@ -0,0 +1,82 @@
+/*bt1 : 1.Trong js đặt sai tên biến sẽ bị báo SyntaxError
+        ví dụ: khai báo 2 biến trùng nhau
+        let mess = "";
+        let mess =""; 
+    số đứng trước ký tự đặt tên
+    let 2name ="";
+    tên có chứa dấu cách
+    let name h ="";
+2. Sử dụng typeof để kiểm tra 
+3. 7 kiểu : number, String, boolean, null, undefined, object, function    
+*/
+
+//bt2:
+/*let msg = 'Rikkei is awesome';
+console.log(msg);
+let studentCount = 0;
+console.log(studentCount);
+//bt 3
+
+msg = 'coding is hard but super cool and fun ';
+console.log(msg);
+
+studentCount =30;
+console.log(studentCount);
+
+console.log(msg.toLocaleUpperCase());
+console.log(msg.toLocaleLowerCase());
+
+let n =parseInt(prompt('nhap n:'));
+n+=3;
+console.log(n);
+*/
+
+//bt4:
+//alert('Hi crush');
+
+//bt5:
+//let name = prompt("what's your name? ");
+//console.log(name);
+
+//alert(`Hi, ${name}`);
+//bt6:
+/*let fistName = prompt("firstName :");
+let middleName = prompt("middleName:");
+let lastName = prompt("lastName: ");
+ let fullName =`${fistName} ${middleName} ${lastName}`;
+console.log(fullName);
+alert (fullName);*/
+
+//bt 8: 
+//let x = parseInt(prompt("Nhap canh hinh vuong:"));
+// let s = x**2;
+
+// alert('dien tich hinh vuong la: '+s)
+
+// let r = parseFloat(prompt("Nhap ban kinh hinh tron:" ));
+// let s = Math.PI* r**2 ;
+
+// alert("Dien tich hinh tron la:"+ s);
+
+//bt9:
+// let c = parseFloat(prompt("Nhập độ C:"));
+
+// let f = (c*1.8)+32;
+
+// alert("Nhiệt độ đổi sang độ F là: "+ f);
+
+//bt10:
+
+// alert('The quick____fox jumps over the____dog');
+// let word = prompt('Nhập từ khoá 1:');
+
+// let word2 = prompt('Nhập từ khoá 2:');
+
+// let fullWords = "The quick bad fox jumps over the mad dog ";
+
+// alert('Đáp án đúng là:'+ fullWords);
+
+//bt 11:
+
+
+ 
